105b43112f87f809dac5c94ca99a3cf8c489a1e2
jolye <elodie.joly.1992@gmail.com>
2013-12-10 13:57:34 +0100

    Create Choix.java
    Déposer la 10/12/2013

---
+++ evenements/Choix.java
@@ -0,0 +1,9 @@
+package monopoly.evenements ;
+
+import monopoly.jeu.Joueur ;
+import monopoly.jeu.Case ;
+
+/** Cette classe est un événement qui permet au joueur de choisir ce qu'il doit faire, elle hérite d'EvenementMonopoly */
+public class Choix extends EvenementMonopoly{
+    // à compléter
+}
