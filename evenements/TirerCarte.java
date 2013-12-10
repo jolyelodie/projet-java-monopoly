123069c4c31ef08c026d12f4cdc397fd675e607f
jolye <elodie.joly.1992@gmail.com>
2013-12-10 13:52:43 +0100

    Create TirerCarte.java
    Déposer le 10/12/2013

---
+++ evenements/TirerCarte.java
@@ -0,0 +1,9 @@
+package monopoly.evenements ;
+
+import monopoly.jeu.Joueur ;
+import monopoly.jeu.Case ;
+
+/** Cette classe permet au joueur de tirer une carte */
+public class TirerCarte extends EvenementMonopoly{
+    // à compléter
+}
