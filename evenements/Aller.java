8b6c41ac4150bdf3263bd571879fa54162b027ff
jolye <elodie.joly.1992@gmail.com>
2013-12-10 13:59:55 +0100

    Create Aller.java
    Déposer le 10/12/2013

---
+++ evenements/Aller.java
@@ -0,0 +1,9 @@
+package monopoly.evenements ;
+
+import monopoly.jeu.Joueur ;
+import monopoly.jeu.Case ;
+
+/** Cette classe est un événement qui permet au joueur de se déplacer */
+public class Aller extends EvenementMonopoly{
+    // à compléter
+}
