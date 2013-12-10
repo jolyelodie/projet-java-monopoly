d8b8eb36f6d4e83a43373d22fb2ec7fe0d4889e4
jolye <elodie.joly.1992@gmail.com>
2013-12-10 14:01:01 +0100

    Create Bonus.java
    Déposer le 10/12/2013

---
+++ evenements/Bonus.java
@@ -0,0 +1,9 @@
+package monopoly.evenements ;
+
+import monopoly.jeu.Joueur ;
+import monopoly.jeu.Case ;
+
+/** Cette classe permet de gérer les bonus */
+public class Bonus extends EvenementMonopoly{
+    // à compléter
+}
