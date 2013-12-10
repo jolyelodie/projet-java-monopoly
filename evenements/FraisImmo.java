19b56b512062f7f9fb162a922f6872c3d13443cc
jolye <elodie.joly.1992@gmail.com>
2013-12-10 13:54:30 +0100

    Create FraisImmo.java
    Déposer le 10/12/2013

---
+++ evenements/FraisImmo.java
@@ -0,0 +1,9 @@
+package monopoly.evenements ;
+
+import monopoly.jeu.Joueur ;
+import monopoly.jeu.Case ;
+
+/** Cette classe permet de gérer les frais immobiliers */
+public class FraisImmo extends EvenementMonopoly{
+    // à compléter
+}
